23451f2d9f7d3aef324a156f381bc2f953263719
Oleksandr <35371181+1UnkMan3@users.noreply.github.com>
2023-02-07 19:09:57 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ const swiper = new Swiper('.swiper-container', {
         prevEl: '.slider-button-prev',
     },
 
+    autoplay: {
+        delay: 5000,
+    },
+
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
